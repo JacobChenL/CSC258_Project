bc3ffd231be7b7bc3cbbbead0d01a52f255cf528
LitaoChen <litao.chen@mail.utoronto.ca>
2020-04-01 19:18:41 -0400

    Complete Initializing Obstacle

--- flappybird.s
+++ flappybird.s
@@ -30,7 +30,6 @@
 # - $t0 = 0 means game is on, $t0 = 1 means game ends
 # - $a1 represents the birdBlock
 # - $a2 represents the direction that bird will move
-# - $a3 represents the
 #####################################################################
 .data 
     displayAddress: .word  0x10008000
@@ -40,21 +39,22 @@
     blue: .word 0x8ac1ff
     birdBlock: .word 1688,1696,1816,1820,1824,1828,1944,1952	# set 1x10008000 as 0, it means the index of unit that bird is at
     green: .word 0x1eb319
-    obstacle: .space 104
+    obstacle: .space 1664
+    newline: .asciiz "\n"
 
 .text
 .globl main
 
 main:
 
-ChangeBackground:
+ChangeBackground:		# $t0, $t1, $t2, $t3 used
   lw $t0, displayAddress
   lw $t1, blue
   li $t2, 0        		# Initialize beginning  
   li $t3, 1024     		# Initialize end  
   
-start_loop_1:  
-  beq $t2, $t3, StartSeting
+start_loop_1:  			# $t0, $t1, $t2, $t3, $t4, $t5 used
+  beq $t2, $t3, INITDONE
   add $t4, $t2, $t2
   add $t4, $t4, $t4
   add $t5, $t4, $t0
@@ -62,22 +62,21 @@ start_loop_1:
   
   addi $t2, $t2, 1    		# Increment counter  
   b start_loop_1     
-
-StartSeting:
-    la $a1, birdBlock		# $a1 = &birdBlock or birdBlock[0]
-    j setBird			# initial the position of bird
+				# $t0, $t1, $t2, $t3, $t4, $t5 freed
+				
 
 INITDONE:
+    li $v0, 42
+    li $a1, 25
+    syscall
+    jal initObstacle
+    la $a1, birdBlock		# $a1 = &birdBlock or birdBlock[0]
+    j setBird			# initial the position of bird
     GAMEINIT:			# A while loop
         li $t0, 0		# $t0 = 0 game continue, $t0 = 1 game ends
         li $v0, 30      	# call getTime(), $a0 = lower 32 bits, $a1 = upper 32 bits
         syscall
         move $t1,$a0		# $t1 = lower 32 bits (in millisecond)
-        li $v0, 42
-        li $a1, 24
-        syscall
-        li $t1, 0
-        j initObstacle
     GAMEON:			
         bne $t0, 0, GAMEOVER	# if $t0 !=0, GAMEOVER
         checkMove1:
@@ -108,14 +107,14 @@ INITDONE:
         syscall
 
 
-setBird:
-    BIRDINIT:
+drawBird:
+    DRAWINIT:
         li $t1, 0		# Index of the loop
         li $t2, 0		# Index of the birdBlock array
         lw $t3, displayAddress	# $t3 = displayAddress
         lw $t4, yellow		# $t4 = yellow
     GETBIRD:
-        beq $t1, 8, DONE	# if $t1 = 8, Done
+        beq $t1, 8, DRAWDONE	# if $t1 = 8, Done
         addi $t1, $t1, 1	# $t1 = $t1 + 1
         add $t5, $t2, $t2	# $t5 = 2 * $t2 (index)
         add $t5, $t5, $t5	# $t5 = 4 * $t2 
@@ -125,8 +124,8 @@ setBird:
         sw $t4, 0($t6)		# load yellow on the bit map at $t5
         addi $t2, $t2, 1	# Index = Index + 1
         j GETBIRD
-    DONE:
-        j INITDONE
+    DRAWDONE:
+        j GAMEINIT
         
 Move:
     REMOVEINIT:
@@ -146,15 +145,15 @@ Move:
         addi $t2, $t2, 1	# Index = Index + 1
         j REMOVEBIRD
     REMOVEDONE:
-        j drop
+        j setBird
         
 
-drop:
-    DROPINIT:
+setBird:
+    SETINIT:
         li $t1,0		# $t1 = 0 is the index of the loop
         la $t2, birdBlock	# $t2 = &birdBlock
-    DROP:
-        bge $t1,8, DROPDONE	# if $t1 >= 8, loop ends
+    SET:
+        bge $t1,8, SETDONE	# if $t1 >= 8, loop ends
         add $t3, $t1,$t1	
         add $t3, $t3, $t3	# $t3 = 4 * $t1 , $t1 is loop index and it is also the index of array
         add $t4, $t3, $t2	# $t4 = &birdBlock + index * 4
@@ -162,28 +161,69 @@ drop:
         add $t5, $t5, $a2	# $t5 = $t5 + 128, same colume, next row
         sw $t5, 0($t4)		# birdBlock[$t1] = $t5, new address
         addi $t1, $t1, 1	# $t1 = $t1 + 1
-        j DROP			
-    DROPDONE:
-        j setBird		# jump to setBird to draw the bird at new position
+        j SET			
+    SETDONE:
+        j drawBird		# jump to setBird to draw the bird at new position
         
-initObstable:
+initObstacle:
     OBINIT:
-        li $t2, 0
-        la $t3, obstacle
-        addi $t4, $a0, 8
+        li $t1, 0		# $t1 = loop index 
+        la $t5, obstacle	# $t5 = &obstacle
+        addi $t6, $a0, 6	# $t6 = $a0 + 8 (lower bound of obstacle)
+        li $t2,0		# $t2 is the index for obstacle list
     GETOB:
-        bge $t2, 32, INITDONE
-        addi $t2, $t2, 1
-        bge $t2, $a0, CHECKEMP
+        bge $t1, 32, drawObstacle	# if $t1 >= 32, jump to INITDONE
+        addi $t1, $t1, 1	# $t1 = $t1 + 1
+        bge $t1, $a0, CHECKEMPT	# if $t1 >= $a0, jump to CHECKEMPT
     OBCONTINUE:
-    	
+    	li $t8, 112		# the column index of 28 on map
+    	li $t9, 128		# the row index (128 bytes per row)
+    	addi $t7, $t1, -1	# we start at row 0, so $t7 = $t1 -1 (row index on the map)
+    	mult $t9, $t7		# row index * row
+    	mflo $t9		# $t9 = row index * row ($t9 is the first byte of row $t7)
+    	add $t8, $t8, $t9	# $t8 is the byte of column 28 at row $t7
+    	li $t9, 16		# $t9 = 16
+    	mult $t9, $t2		# 16 * row ( This is the byte of every 4 index in obstacle, we input 4 units at the same time)
+    	mflo $t9		# $t9 = 16 * row
+    	li $t7,0		# $t7 is loop index
+    ADDLOOP:			# This loop is setting the bytes for each row of map into obstacle 
+    	bge $t7, 4, ENDADDLOOP	# if $t7 = 4, jump to ENDADDLOOP
+    	add $t3, $t7, $t7	# $t3 = 2 * $t7
+    	add $t3, $t3,  $t3	# $t3 = 4 * $t7
+    	add $t4, $t3, $t8	# $t4 represent colume 28,29,30,31 on a row ( on map)
+    	add $t0, $t9, $t3	# $t0 = index of obstacle, every 4 index + 0,1,2,3
+    	add $t0, $t0, $t5	# $t0 is the address on obstacle of index [4*row + $t7]
+    	sw $t4, 0($t0)		# load $t4 to obstacle [4*row + $t7]
+    	addi $t7, $t7, 1	# loop index $7 = $t7 + 1
+    	j ADDLOOP
+    ENDADDLOOP:
+        addi $t2, $t2,1		# obstacle index + 1
+        j GETOB
+    CHECKEMPT:
+        blt $t1, $t6, GETOB	# if $t3 <= $t4 < $t6, jump to GETOB. Means the current row is empty.
+        j OBCONTINUE		# if $t4 >= $t6, jumpty to OBCONTINUE
     
+drawObstacle:
     
-    CHECKEMP:
-        blt $t2, $t4, GETOB
-        j OBCONTINUE
-    
-    
-
+    DOBINIT:
+        lw $t0, displayAddress	# $t0 = &displayAddress[0]
+    	li $t4, 0		# loop index $t4 = 0
+    	la $t5, obstacle	# $t5 = &obstacle
+    	lw $t6, green
+    DOBLOOP:
+        beq $t4, 104, DOBDONE	# if $t4 == len(Obstacle), jump to DOBDONE
+        li $t7, 4		# $t7 = 4
+        mult $t4, $t7		# index * 4 ( the byte of index $t4)
+        mflo $t7		# $t7 = index * 4
+        add $t7, $t5, $t7	# $t7 = &Obstacle[$t4]
+        lw $t8, 0($t7)		# $t8 = value of Obstacle[$t4]
+        
+        add $t8, $t8, $t0	# $t8 = a certain index on displayAddress
+        sw $t6, 0($t8)		# load green color onto the index of displayAddress
+        addi $t4,$t4,1		# index $t4 = $t4 + 1
+        j DOBLOOP		
+     DOBDONE:
+         jr $ra			# return to main code
+        
         
     
